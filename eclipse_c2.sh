052fc30459d3dc3f7de9f0fbdabdd6ccbf41e770
WebDragon <117004849+webdragon63@users.noreply.github.com>
2025-03-19 06:08:22 +0000

    Update eclipse_c2.sh

--- eclipse_c2.sh
+++ eclipse_c2.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# Find the Previous PIDs of web_delivery processes
+pids=$(pgrep -f "web_delivery")
+
+# Check if any PIDs were found
+if [ -n "$pids" ]; then
+    echo "Killing previous web_delivery processes: $pids"
+    kill -9 $pids
+else
+    echo "No web_delivery processes found."
+fi
+
+
 # Define futuristic border elements
 clear
 TOP_LEFT="╭─╼"
