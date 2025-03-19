de58291177fdef1e447361233e9f99eb4662afd2
WebDragon <117004849+webdragon63@users.noreply.github.com>
2025-03-19 08:03:01 +0000

    Update pkiller.sh

--- pkiller.sh
+++ pkiller.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+if [ "$EUID" -ne 0 ]; then
+    echo "This script must be run as root."
+    exit 1
+fi
 
 # Find the PIDs of web_delivery processes
 pids=$(pgrep -f "web_delivery")
@@ -10,4 +14,3 @@ if [ -n "$pids" ]; then
 else
     echo "No web_delivery processes found."
 fi
-
